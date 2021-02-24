eda212f4a9a7994aedaf010f49d1e6a66fb8a722
Marion-H <doudou6500@gmail.com>
2021-02-24 13:36:35 +0100

    add new librairy for icons and add css for input

--- src/components/FormAuth.jsx
+++ src/components/FormAuth.jsx
@@ -1,16 +1,34 @@
-import "./css/formAuth.scss"
+import { FaEye } from "react-icons/fa";
+import { RiLockPasswordFill } from "react-icons/ri";
+import { BsFillPersonFill } from "react-icons/bs";
+import "./css/formAuth.scss";
+
+// FaEyeSlash
 
 function FormAuth() {
-    return(
-        <div className="formAuth">
-            <form>
-                <input />
-                <input />
-                <span>Forgot your password ?</span>
-                <button>Sign in</button>
-            </form>
+  return (
+    <div className="formAuth">
+      <form>
+        <div className="inputName">
+          <span className="fillPerson">
+            <BsFillPersonFill />
+          </span>
+          <input />
         </div>
-    )
+        <div className="inputPassword">
+          <span className="lockPassword">
+            <RiLockPasswordFill />
+          </span>
+          <input />
+          <span class="eyePassword">
+            <FaEye />
+          </span>
+        </div>
+        <span>Forgot your password ?</span>
+        <button>Sign in</button>
+      </form>
+    </div>
+  );
 }
 
 export default FormAuth;
