8758e630f9880825be3b92d16e09eb1f86855fd0
Marion-H <doudou6500@gmail.com>
2021-02-26 14:15:04 +0100

    add a function for submit form send mail for reset password

--- src/components/FormResetPassword.jsx
+++ src/components/FormResetPassword.jsx
@@ -12,12 +12,16 @@ function FormResetPassword() {
   const { email } = dataForm
   const history = useHistory()
 
+  const postEmailForReset = (e) => {
+    e.preventDefault()
+      return history.push("/reset")
+  }
   
   return (
     <div className="formAuth">
-      <form onSubmit="">
+      <form onSubmit={postEmailForReset}>
         <InputForm type="email" icon="aiOutlineMail" keyName="email" textPlaceHolder="Email"/>
-        <ButtonSign text="Reset password" />
+        <ButtonSign text="Reset password" type="submit" />
       </form>
     </div>
   );
