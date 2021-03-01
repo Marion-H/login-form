8cc2eaf995b88b493df9fc90bd5b89f78fc6568e
Marion-H <doudou6500@gmail.com>
2021-03-01 14:32:24 +0100

    add a new component change password

---
+++ src/components/FormPasswordChange.jsx
@@ -0,0 +1,9 @@
+function FormPasswordChange(){
+    return (
+        <div>
+            hello
+        </div>
+    )
+}
+
+export default FormPasswordChange
--- src/components/ResetPasswordEmail.jsx
+++ src/components/ResetPasswordEmail.jsx
@@ -1,9 +1,22 @@
-function ResetPasswordEmail () {
-    return (
-        <div>
-            hello
-        </div>
-    )
+import { Link } from "react-router-dom";
+
+function ResetPasswordEmail() {
+  return (
+    <div className="authentication">
+      <div className="titleReset">
+        <h1>Reset Password</h1>
+      </div>
+      <div className="paragraphReset">
+        Check your inbox for the next steps. If you don't receive an email, and
+        it's not in your spam folder this could mean you signed up with a
+        different address.
+      </div>
+
+      <div className="textFooterForm">
+      <Link to="/">Log in</Link> or <Link to="/create">Sign up</Link>
+      </div>
+    </div>
+  );
 }
 
-export default ResetPasswordEmail
+export default ResetPasswordEmail;
