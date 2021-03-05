197239e2ab04f349da941ddafdde55c02f8974bf
Marion-H <doudou6500@gmail.com>
2021-03-05 15:18:35 +0100

    fix bug apiurl

--- src/variable/apiUrl.js
+++ src/variable/apiUrl.js
@@ -3,7 +3,7 @@ let apiUrl;
 if (process.env.NODE_ENV === "development") {
   apiUrl = "http://localhost:8080"
 } else {
-  apiUrl = "https://login-form-back.marion.hourdou.dev/"
+  apiUrl = "https://login-form-back.marion.hourdou.dev"
 }
 
 export default apiUrl
