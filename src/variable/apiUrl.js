2dc5134594b2377ffeca7fed0015446d759377b7
Marion-H <doudou6500@gmail.com>
2021-03-05 15:01:10 +0100

    add api Url

--- src/variable/apiUrl.js
+++ src/variable/apiUrl.js
@@ -2,6 +2,8 @@ let apiUrl;
 
 if (process.env.NODE_ENV === "development") {
   apiUrl = "http://localhost:8080"
+} else {
+  apiUrl = "https://login-form-back.marion.hourdou.dev/"
 }
 
 export default apiUrl
