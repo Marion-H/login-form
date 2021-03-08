ce0e9106c221ecbc749188f52eb8e559c8ed2b45
Marion-H <doudou6500@gmail.com>
2021-03-08 17:29:30 +0100

    add function postEmail for send email to user

--- src/components/FormResetPassword.jsx
+++ src/components/FormResetPassword.jsx
@@ -5,16 +5,22 @@ import InputForm from "./common/InputForm";
 import ButtonSign from "./common/ButtonSign";
 
 import "./scss/formAuth.scss";
+import Axios from "axios";
+import apiUrl from "../variable/apiUrl";
 
 function FormResetPassword() {
 
-  // const dataForm = useSelector((state) => state.dataForm);
-  // const { email } = dataForm
+  const dataForm = useSelector((state) => state.dataForm);
+  const { email } = dataForm
   const history = useHistory()
 
-  const postEmailForReset = (e) => {
+  const postEmailForReset = async (e) => {
     e.preventDefault()
-      return history.push("/check-email")
+      const emailSend = await Axios.post (`${apiUrl}/recover`, {
+        email,
+      })
+      console.log(emailSend)
+      await history.push("/check-email")
   }
   
   return (
