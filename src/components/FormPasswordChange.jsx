0a0efa88ca7ae5d40e797da71b835ad6f9a94b3f
Marion-H <doudou6500@gmail.com>
2021-03-11 15:13:50 +0100

    add a history push when user valid new password and a renderMessageUnderInput

--- src/components/FormPasswordChange.jsx
+++ src/components/FormPasswordChange.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
-import { useParams } from "react-router-dom";
+import { useHistory, useParams } from "react-router-dom";
 import Axios from "axios";
 
 import ButtonSign from "./common/ButtonSign";
@@ -11,18 +11,22 @@ import { notifyError, notifySuccess } from "./common/toastifyFunction";
 function FormPasswordChange() {
   const { resetPasswordToken } = useParams();
   const [pwdIsSame, setPwsIsSame] = useState(false);
+  const [checkRegExPwdStrong, setCheckRegExPwdStrong] = useState(false);
   const changePassword = useSelector((state) => state.changePassword);
   const { newPassword, confirmPassword } = changePassword;
+  const strongRegExPwd = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/i; //eslint-disable-line
+  const history = useHistory();
 
   useEffect(() => {
+    setCheckRegExPwdStrong(strongRegExPwd.test(newPassword));
     if (newPassword.length > 0) {
-      if (newPassword === confirmPassword) {
+      if (newPassword === confirmPassword && checkRegExPwdStrong) {
         return setPwsIsSame(true);
       } else {
         setPwsIsSame(false);
       }
     }
-  }, [newPassword, confirmPassword, pwdIsSame]);
+  }, [newPassword, confirmPassword, pwdIsSame, checkRegExPwdStrong, strongRegExPwd]);
 
   const postNewPassword = async (e) => {
     e.preventDefault();
@@ -30,11 +34,26 @@ function FormPasswordChange() {
       await Axios.post(`${apiUrl}/reset/${resetPasswordToken}`, {
         newPassword,
       });
-      const message = "your password has been reset"
-      notifySuccess(message)
+      const message = "your password has been reset";
+      notifySuccess(message);
+      history.push("/validation")
     } catch (error) {
       const { message } = error.response.data;
-      notifyError(message)
+      notifyError(message);
+    }
+  };
+
+  const renderMessageUnderInput = () => {
+    if (newPassword.length > 0) {
+      if (newPassword !== confirmPassword) {
+        return <span>Passwords are not the same</span>;
+      } else if (newPassword === confirmPassword && !checkRegExPwdStrong) {
+        return (
+          <span>
+            Password must have a minimum 8 characters, a number and a symbol
+          </span>
+        );
+      }
     }
   };
 
@@ -57,7 +76,7 @@ function FormPasswordChange() {
             keyName="confirmPassword"
             placeholder="Confirm New Password"
           />
-          {!pwdIsSame && <p>Passwords are not the same</p>}
+          {renderMessageUnderInput()}
           <ButtonSign
             text="Reset password"
             type="submit"
